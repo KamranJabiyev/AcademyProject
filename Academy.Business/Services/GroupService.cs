739eff072239e95f74101870493cc8de84420540
KamranJabiyev <kamran.jabiyev@code.edu.az>
2023-12-21 15:10:18 +0400

    modified Category Create method

--- Academy.Business/Services/GroupService.cs
+++ Academy.Business/Services/GroupService.cs
@@ -1,16 +1,23 @@
 ﻿using Academy.Business.Interfaces;
+using Academy.Business.Utilities.Exceptions;
 using Academy.Core.Entities;
+using Academy.DataAccess.Contexts;
 
 namespace Academy.Business.Services;
 
 public class GroupService : IGroupService
 {
-    public void Activate(string name, bool activated)
-    {
-        throw new NotImplementedException();
-    }
-
     public void Create(string name, int maxStudentCount, string categoryName)
+    {
+        if (String.IsNullOrEmpty(name)) throw new ArgumentNullException();
+        Group? dbGroup =
+            AcademyDbContext.Groups.Find(c => c.Name.ToLower() == name.ToLower());
+        if (dbGroup is not null)
+            throw new AlreadyExistException($"{dbGroup.Name} is already exist");
+        if (maxStudentCount < 4)
+            throw new MinCountException("Minimum student count requirement is 4");
+    }
+    public void Activate(string name, bool activated)
     {
         throw new NotImplementedException();
     }
